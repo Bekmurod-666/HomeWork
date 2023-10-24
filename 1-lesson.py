9e378e9f88a0a84677e93766112a37240212bb60
Bekmurod-666 <148315303+Bekmurod-666@users.noreply.github.com>
2023-10-24 18:05:03 +0500

    Create 1-lesson.py

---
+++ 1-lesson.py
@@ -0,0 +1,8 @@
+def son_kirit(son):
+    print(f"Kvadrati: {son ** 2}\n"
+          f"Kubi: {son ** 3}\n"
+          f"Son: {son % 2 == 0} juft\n"
+          f"Son: {son % 2 == 1} toq")
+
+
+son_kirit(14)
